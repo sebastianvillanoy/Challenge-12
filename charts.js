f528147482c664d92b8be0a9ad91e399f7de79a1
Sean Villanoy <seanvillanoy@Seans-MacBook-Pro.local>
2021-01-27 14:48:09 -0500

    Finished Deliverable 2

--- charts.js
+++ charts.js
@@ -120,6 +120,31 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout)
-    
+
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otuIds,
+      y: sampleValues,
+      text: otuLabels,
+      mode: 'markers',
+      marker:{ 
+        size: sampleValues,
+        color: otuIds,
+        colorscale: "Earth"
+      }
+    }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title:"Bacteria Cultures Per Sample",
+      xaxis:{title: "OTU ID"},
+      yaxis:{title: "Sample Values"},
+      hovermode: "closest",
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout)
+
   });
 }
